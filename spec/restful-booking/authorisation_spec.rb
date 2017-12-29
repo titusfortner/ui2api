0157b1cfa09e43051af571b7bed6fc358615379d
Titus Fortner <titusfortner@gmail.com>
2017-12-29 11:38:53 -0600

    tidy authorisation spec

--- spec/restful-booking/authorisation_spec.rb
+++ spec/restful-booking/authorisation_spec.rb
@@ -1,11 +1,9 @@
 RSpec.describe WatirApi do
-  describe "#authorisation" do
-
-    it "returns authorisation" do
+  describe "Authorisation" do
+    it "returns user data" do
       user = Model::User.authorised.to_api
       authenticate = API::Authenticate.create(user)
       expect(JSON.parse authenticate.body).to have_key('token')
     end
-
   end
 end
