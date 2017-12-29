328a89c68263205475b19b0eed0f5aa071ab66a3
Titus Fortner <titusfortner@gmail.com>
2017-12-29 11:34:23 -0600

    enforce data types for conversions

--- spec/support/data/booking.rb
+++ spec/support/data/booking.rb
@@ -1,16 +1,16 @@
 module WatirApi
   module Model
     class BookingDates < Base
-      key(:checkin) { Date.today + 1 }
-      key(:checkout) { Date.today + 7 }
+      key(:checkin, data_type: Date) { Faker::Date.forward }
+      key(:checkout, data_type: Date) { checkin + 4 }
     end
 
     class Booking < Base
       key(:firstname) { Faker::Name.first_name }
       key(:lastname) { Faker::Name.last_name }
-      key(:totalprice) { Faker::Commerce.price }
+      key(:totalprice, data_type: Integer) { Faker::Commerce.price.round }
       key(:depositpaid) { true }
-      key(:bookingdates) { BookingDates.new }
+      key(:bookingdates, data_type: BookingDates) { BookingDates.new }
       key(:additionalneeds) { 'Breakfast' }
     end
   end
