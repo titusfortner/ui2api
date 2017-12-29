eedcddc87ac63b700013b4b24f8734b0d87b1918
Titus Fortner <titusfortner@gmail.com>
2017-12-29 12:21:11 -0600

    allow overriding of #model_object

--- spec/support/apis/booking.rb
+++ spec/support/apis/booking.rb
@@ -1,18 +1,23 @@
 module API
   class Booking < WatirApi::Base
-    def self.endpoint
-      'booking'
-    end
+    class << self
+      def endpoint
+        'booking'
+      end
 
-    def self.update(id:, with:, **opt)
-      opt[:cookies] = {token: opt.delete(:token)}
-      super
-    end
+      def update(id:, with:, **opt)
+        opt[:cookies] = {token: opt.delete(:token)}
+        super
+      end
 
-    def self.destroy(opt)
-      opt[:cookies] = {token: opt.delete(:token)}
-      super
+      def destroy(opt)
+        opt[:cookies] = {token: opt.delete(:token)}
+        super
+      end
+
+      def model_object
+        Model::Booking
+      end
     end
   end
-
 end
